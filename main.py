54ef7e52c4a0435f053ef34803240a70f9b1113d
tensey <tensey26@gmail.com>
2026-02-22 18:10:26 +0200

    add TaskManager class structure

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ class Task:
         return f"[{self.id}] {self.header} | status: {self.status} | created_at: {self.created_at}"
 
 
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, header: str, description: str):
+
+        if len(self.tasks) == 0:
+            new_id = 1
+        else:
+            new_id = max([task.id for task in self.tasks]) + 1
+        
+        new_task = Task(new_id, header, description)
+        self.tasks.append(new_task)
+
+    def show_tasks(self):
+        if len(self.tasks) == 0:
+            print("tasks not found")
+        else:
+            for task in self.tasks:
+                print(task)
+
+
